2628053655a002e30039bb8a1e321f3975c3ed87
yuyuko <ncysatnaf@gmail.com>
2018-11-05 12:13:31 +0800

    update config

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 # cd ./admin; yarn install;
 # cd ../poi; npm install; npm run build;
-cd ../; docker-compose down; docker-compose up -d
+cd ../; docker-compose down; docker-compose -f docker-compose-prod.yml up -d
